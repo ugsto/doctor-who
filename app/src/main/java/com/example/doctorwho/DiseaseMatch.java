3992ffe80116f1a9c3fd4cefde5548c016ef8eee
André Bortoli <andre@bortoli.phd>
2024-11-28 18:51:08 -0300

    db

---
+++ app/src/main/java/com/example/doctorwho/DiseaseMatch.java
@@ -0,0 +1,11 @@
+package com.example.doctorwho;
+
+public class DiseaseMatch {
+    public final String diseaseName;
+    public final int matchCount;
+
+    public DiseaseMatch(String diseaseName, int matchCount) {
+        this.diseaseName = diseaseName;
+        this.matchCount = matchCount;
+    }
+}
